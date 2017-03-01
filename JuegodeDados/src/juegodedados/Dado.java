414a1b1ed33e4e01236b7c5d314d4c8d43394881
estudiantes <estudiantes@labing00>
2017-03-01 16:45:38 -0500

    creación de objetos clase dados

---
+++ JuegodeDados/src/juegodedados/Dado.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegodedados;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class Dado {
+    
+      
+    private float dado1;
+    private float dado2;
+
+    public float getDado1() {
+        return dado1;
+    }
+
+    public void setDado1(float dado1) {
+        this.dado1 = dado1;
+    }
+
+    public float getDado2() {
+        return dado2;
+    }
+
+    public void setdado2(float dado2) {
+        this.dado2 = dado2;
+    }
+
+    
+}
